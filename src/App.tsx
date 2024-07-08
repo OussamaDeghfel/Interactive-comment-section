1dc1cb65575d8bc2f44570a1a2559fbda42e4c76
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-07-08 15:36:49 +0100

    import comment component to each comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import Comment from "./components/Comment"
-
+import Comment from "./components/Comment";
+import MyComment from "./components/MyComment";
 
 function App() {
-
   return (
     <>
-      <Comment />
+      <div className="flex flex-col justify-center items-center h-screen m-auto">
+        <Comment
+          userName={"oussama"}
+          comment={
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel facilis totam quo eius numquam ab pariatur hic non voluptas consequatur?"
+          }
+        />
+        <MyComment />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
