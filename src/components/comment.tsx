457e35f702b30bda2fc0219afb912bd6f7b34fed
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-07-08 13:35:43 +0100

    add all the comment needed information

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -1,15 +1,28 @@
 import { useState } from 'react'
-import { FaMinus, FaPlus } from 'react-icons/fa'
+import { FaMinus, FaPlus, FaReply } from 'react-icons/fa'
+import mbappe from '../assets/mbappe-pic.png'
 
 const Comment = () => {
   const [vote, setVote] = useState(0)
   return (
-    <div>
-      <div className='bg-gray-400 rounded-md flex flex-col w-[45px] h-full p-4 justify-between'>
-        <FaPlus onClick={() => setVote(vote+1)} size={10} />
-        <span className='text-2xl'>{vote}</span>
-        <FaMinus onClick={() => setVote(vote+1)} size={10} />
+    <div className='p-5 flex shadow-md border-gray-500 rounded-md w-fit m-5'>
+      <div className='bg-gray-400 rounded-md flex flex-col w-[30px] h-full p-2 justify-around'>
+        <FaPlus onClick={() => setVote(vote+1)} size={10} className='text-gray-500' />
+        <span className='text-xl py-2 text-blue-950'>{vote}</span>
+        <FaMinus onClick={() => setVote(vote+1)} size={10} className='text-gray-500' />
       </div>
+      <div className='flex flex-col pl-4'>
+        <div className='flex'>
+        <img src={mbappe} alt="user-image" className='w-[30px] h-[30px] rounded-full' />
+        <h1 className='font-bold pl-2'>Mbappe</h1>
+        <h2 className='pl-2'>1 month ago</h2>
+        </div>
+        <p className='w-[550px] pt-2 text-gray-600'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo tempora odit minus! Sunt non minus veritatis reiciendis perferendis commodi esse.</p>
+      </div>
+      <button className='flex text-blue-900 space-x-2 items-center'>
+        <FaReply size={15} /> 
+        <span className='font-bold'>Reply</span>
+      </button>
     </div>
   )
 }
