865748986dddd4f719ffe262d6410107d5a62eb8
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-07-09 12:30:00 +0100

    change the form of reply from send form to reply form

--- src/components/ReplyComment.tsx
+++ src/components/ReplyComment.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import MyComment from './MyComment'
+import React from "react";
+import MyComment from "./MyComment";
+import leva from "../assets/leva-pic.png";
 
 const ReplyComment = () => {
-    
   return (
     <div>
-        <MyComment />
+      <form>
+        <div className="flex space-x-5 items-start">
+          <img src={leva} alt="" className="w-[40px] h-[40px] rounded-full" />
+          <textarea
+            placeholder="Add a comment..."
+            className="w-[550px] h-[100px] flex items-start border-2 border-gray-200 rounded-md p-2"
+          />
+          <button className="text-white bg-blue-900 rounded-md p-4 font-medium">
+            Reply
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ReplyComment
+export default ReplyComment;
