0db45e59466c678a9e250b0e7fa691e88f0578d6
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-07-09 11:31:12 +0100

    replace div by form

--- src/components/MyComment.tsx
+++ src/components/MyComment.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import leva from "../assets/leva-pic.png"
+import React from "react";
+import leva from "../assets/leva-pic.png";
 
 const MyComment = () => {
   return (
-    <div className='flex space-x-5 items-start'>
-        <img src={leva} alt="" className='w-[30px] h-[30px] rounded-full'/>
-        <input type="text" placeholder='Add a comment...' className='w-[550px] h-[100px] flex items-start border-2 border-gray-200 rounded-md p-2' />
-        <button className='p-2 bg-blue-900 rounded-md font-medium text-white'>Send</button>
-    </div>
-  )
-}
+    <form>
+      <div className="flex space-x-5 items-start">
+        <img src={leva} alt="" className="w-[40px] h-[40px] rounded-full" />
+        <textarea
+          placeholder="Add a comment..."
+          className="w-[550px] h-[100px] flex items-start border-2 border-gray-200 rounded-md p-2"
+        />
+        <button className="text-white bg-blue-900 rounded-md p-4 font-medium">Send</button>
+      </div>
+    </form>
+  );
+};
 
-export default MyComment
+export default MyComment;
