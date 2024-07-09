2655448ad6d767bb81dd73561bd66ba2a152f5ab
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-07-09 12:29:30 +0100

    use mapping in state list to show comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
+import { useState } from "react";
 import Comment from "./components/Comment";
 import MyComment from "./components/MyComment";
 
 function App() {
+  const [commentList, setCommentList] = useState([
+    {
+      id: 1,
+      userName: "oussama",
+      comment:
+        "Lorem ipsum dolor sit amet ng elit. Nulla, minus. Minus praesentium ea eius placeat omnis rem sit blanditiis sed!",
+    },
+    {
+      id: 2,
+      userName: "Mbappe",
+      comment:
+        "Lorem ipsum dolor siminus. Minus praesentium ea eius placeat omnis rem sit blanditiis sed!",
+    },
+    {
+      id: 3,
+      userName: "Halland",
+      comment: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    },
+  ]);
+
   return (
     <>
       <div className="flex flex-col justify-center items-center h-screen m-auto">
-        <Comment
+        {/* <Comment
           userName={"oussama"}
           comment={
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel facilis totam quo eius numquam ab pariatur hic non voluptas consequatur?"
           }
-        />
+        /> */}
+
+        {commentList.map((comment: any) => (
+          <Comment userName={comment.userName} comment={comment.comment} />
+        ))}
         <MyComment />
       </div>
     </>
