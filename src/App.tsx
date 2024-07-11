b7d87fd49bbbd6155ee514753624bbf9f74bff6d
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-07-11 16:59:42 +0100

    add replies to each comment

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Comment from "./components/Comment";
 import AddComment from "./components/AddComment";
 
 interface commentType {
-  id: number
-  date: number
-  userName: string
-  comment: string
-  replies?: commentType[]
+  id: number;
+  date: number;
+  userName: string;
+  comment: string;
+  replies?: commentType[];
 }
 
 function App() {
@@ -18,28 +18,28 @@ function App() {
       date: 1,
       comment:
         "Lorem ipsum dolor sit amet ng elit. Nulla, minus. Minus praesentium ea eius placeat omnis rem sit blanditiis sed!",
-    } as commentType
+      replies: [],
+    } as commentType,
   ]);
 
   function handleAddComment(newComment: commentType) {
-  
     setCommentList([...commentList, newComment]);
-
   }
+
 
   return (
     <>
       <div className="flex flex-col justify-center items-center h-full m-auto">
-        {/* <Comment
-          userName={"oussama"}
-          comment={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel facilis totam quo eius numquam ab pariatur hic non voluptas consequatur?"
-          }
-        /> */}
-
         {commentList.map((comment: commentType) => (
-          <Comment id={comment.id} userName={comment.userName} comment={comment.comment} date={comment.date} />
+          <Comment
+            id={comment.id}
+            userName={comment.userName}
+            comment={comment.comment}
+            date={comment.date}
+            replies={comment.replies}
+          />
         ))}
+
         <AddComment addComment={handleAddComment} />
       </div>
     </>
