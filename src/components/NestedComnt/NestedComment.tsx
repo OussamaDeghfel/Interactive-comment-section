ca2fc7055757ebeb613efd5c64102d5623a21c86
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-07-15 12:03:11 +0100

    add state of edit to show save and cancel button when user try to edit the comment

--- src/components/NestedComnt/NestedComment.tsx
+++ src/components/NestedComnt/NestedComment.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
+import Action from "./Action";
 
 const NestedComment = ({ comment }) => {
   const [inputComment, setInputComment] = useState("");
+  const [edit, setEdit] = useState(false);
 
   function onAddComment() {}
 
   return (
     <div>
-      <div className="flex">
+      <div>
         {comment.id === 1 ? (
-          <>
+          <div className="flex">
             <input
               type="text"
               placeholder="type comment..."
@@ -23,9 +25,27 @@ const NestedComment = ({ comment }) => {
             >
               comment
             </div>
-          </>
+          </div>
         ) : (
-          <span className="bg-gray-500 rounded-md p-2 m-2 text-white">{comment.content}</span>
+          <div className="grid">
+            <span className="bg-gray-500 rounded-md p-2 m-2 text-white">
+              {comment.content}
+            </span>
+            <div className="flex">
+              {edit ? (
+                <>
+                  <Action type="Save" />
+                  <Action type="Cancel" handleClick={() => setEdit(false)} />
+                </>
+              ) : (
+                <>
+                  <Action type="Reply" />
+                  <Action type="Edit" handleClick={() => setEdit(true)} />
+                  <Action type="Delete" />
+                </>
+              )}
+            </div>
+          </div>
         )}
       </div>
       <div className="ml-4 ">
