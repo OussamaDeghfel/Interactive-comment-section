23784162ea4e771675fffd3afbabcfe4ce286442
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-07-15 11:52:48 +0100

    add intial data to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,69 @@
 import { useEffect, useState } from "react";
 import Comment from "./components/Comment";
 import AddComment from "./components/AddComment";
+import ReplyComment from "./components/ReplyComment";
+import NestedComment from "./components/NestedComnt/NestedComment";
 
-interface commentType {
-  id: number;
-  date: number;
-  userName: string;
-  comment: string;
-  replies?: commentType[];
-}
+// interface commentType {
+//   id: number;
+//   // date: number;
+//   // userName: string;
+//   content: string;
+//   replies?: commentType[];
+// }
 
-function App() {
-  const [commentList, setCommentList] = useState([
+const comments = {
+  id: 1,
+  replies: [
     {
-      id: 1,
-      userName: "oussama",
-      date: 1,
-      comment:
-        "Lorem ipsum dolor sit amet ng elit. Nulla, minus. Minus praesentium ea eius placeat omnis rem sit blanditiis sed!",
-      replies: [],
-    } as commentType,
-  ]);
+      id: 124578,
+      content: "kirak Dayer",
+      replies: [
+        {
+          id: 3,
+          content: "kach sport",
+          replies: [],
+        },
+      ],
+    },
+    {
+      id: 125487,
+      content: "thala fi your health",
+      replies: [{
+        id: 125847,
+        content: "thala",
+        replies: [],
+      },],
+    },
+  ],
+};
+function App() {
+  const [commentList, setCommentList] = useState(comments);
 
-  function handleAddComment(newComment: commentType) {
-    setCommentList([...commentList, newComment]);
-  }
-
-  useEffect(() => {
-    localStorage.setItem("commentList", JSON.stringify(commentList))
-  }, [commentList])
-
-  useEffect(() => {
-    const commentList = JSON.parse(localStorage.getItem("commentList") || "[]")
-    setCommentList(commentList)
-  },[])
-
+  // function handleAddComment(newComment: commentType) {
+  //   setCommentList([...commentList, newComment]);
+  // }
 
   return (
     <>
       <div className="flex flex-col justify-center items-center h-full m-auto">
-        {commentList.map((comment: commentType) => (
+        {/* {commentList.map((commentList: commentType) => (
+          <>
           <Comment
-            id={comment.id}
-            userName={comment.userName}
-            comment={comment.comment}
-            date={comment.date}
-            replies={comment.replies}
+            // id={comment.id}
+            // userName={comment.userName}
+            // comment={comment.comment}
+            // date={comment.date}
+            // replies={comment.replies}
+            commentList={commentList}
           />
-        ))}
+          <AddComment addComment={handleAddComment} />
+          </>
+))}
 
-        <AddComment addComment={handleAddComment} />
+        <AddComment addComment={handleAddComment} /> */}
+
+        <NestedComment comment={commentList} />
       </div>
     </>
   );
