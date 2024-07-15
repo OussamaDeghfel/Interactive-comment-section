e20e335f043360b1ece4a102205c8108adc37419
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-07-15 11:53:16 +0100

    define action button and import it in app.tsx to use it

---
+++ src/components/ActionButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ActionButton = () => {
+  return (
+    <div>
+        <button>
+            Add Comment
+        </button>
+    </div>
+  )
+}
+
+export default ActionButton
